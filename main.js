315d60ac1f40965c9eb04ae04f80fa0f397e428b
Vladimir_Novikov <vovasnew@mail.ru>
2020-12-19 16:22:01 +0500

    Сделал все задания. 3 и 4 в отдельных файлах

---
+++ main.js
@@ -0,0 +1,18 @@
+// задание 1
+var tempC = 25;
+var tempF = (9 / 5) * tempC + 32;
+alert('Задание 1\nТемпература ' + tempC + ' градусов по Цельсию равна ' + tempF + ' градусов по Фаренгейту');
+
+// задание 2
+var admin;
+var Name;
+Name = 'Владимир'
+admin = Name;
+alert('Задание 2\n' + admin);
+
+
+// задание 3
+var first = 1000;
+var second = "108";
+total = first + second;
+alert('JS произвел приведение типов, и мы получили конкатенацию\nстрок, т.е. первое число JS привел к типу данных String\n' + total);
